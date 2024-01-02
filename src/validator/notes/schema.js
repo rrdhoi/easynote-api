aeb8735fe85d222ad5b02a356e274199ea647a38
Muhammad Ridhoi <muhammad.ridhoi@borwita.co.id>
2024-01-02 16:32:13 +0700

    first commit

---
+++ src/validator/notes/schema.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const joi = require('joi');
+
+const NotePayloadSchema = joi.object({
+  title: joi.string().required(),
+  body: joi.string().required(),
+  tags: joi.array().items(joi.string()).required(),
+});
+
+module.exports = { NotePayloadSchema };
